b21dd298827d9bba476dd4a8e7ff177f0d5c0691
Harry Wang <harryjwang@gmail.com>
2022-01-18 19:48:55 -0500

    remove backtick and quote

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,27 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": false,
+            "code::after": false,
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+          },
+        },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
+
   variants: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
   ],
+
 }
